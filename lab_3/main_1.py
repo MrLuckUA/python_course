ef94d14abceb3b9a75220fefc0a9098c4bc0b706
MrLuckUA <mrluck707@gmail.com>
2018-09-10 12:21:37 +0300

    create lab3

---
+++ lab_3/main_1.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+# -*-coding: utf-8-*-
+print(int(input('Enter first numb:')) + int(input('Enter second numb:')))
---
+++ lab_3/main_2.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+# -*-coding: utf-8-*-
+print(f'Temperature in C: {(int(input("Enter temperature in F: ")) - 32) * 5/9}')
